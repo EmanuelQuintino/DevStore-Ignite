674953c84cd8cfb077b654846483a5ab91902b0c
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-10-11 09:06:20 -0300

    add api products route

---
+++ src/app/api/products/featured/route.ts
@@ -0,0 +1,6 @@
+import data from "../data.json";
+
+export async function GET() {
+  const featuredProducts = data.products.filter((product) => product.featured);
+  return Response.json(featuredProducts);
+}
