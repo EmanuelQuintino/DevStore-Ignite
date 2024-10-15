22cfeb04023861a9e2327a918cb53af3958d1450
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-10-15 11:17:38 -0300

    add get page product

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,27 +1,57 @@
 import Image from "next/image";
-export default function ProductPage() {
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
+
+interface ProductProps {
+  params: {
+    slug: string;
+  };
+}
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60 * 2, // 2 hour
+    },
+  });
+
+  const product = await response.json();
+
+  return product;
+}
+
+export default async function ProductPage({ params }: ProductProps) {
+  const product = await getProduct(params.slug);
+
   return (
     <div className="relative grid max-h-[860px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
-        <Image
-          src="/moletom-never-stop-learning.png"
-          alt=""
-          width={720}
-          height={720}
-          quality={100}
-        />
+        <Image src={product.image} alt="" width={720} height={720} quality={100} />
       </div>
+
       <div className="flex flex-col justify-center px-12">
-        <h1 className="text-3xl font-bold leading-tight">Moletom Never Stop Learning</h1>
-        <p className="mt-2 leading-relaxed text-zinc-400">
-          Moletom fabricado com 88% de algodão e 12% de poliéster
-        </p>
+        <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
+
+        <p className="mt-2 leading-relaxed text-zinc-400">{product.description}</p>
+
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            R$129
+            {product.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "brl",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
           </span>
-          <span className="text-sm text-zinc-400">Em 12x s/ juros de R$10,75</span>
+          <span className="text-sm text-zinc-400">
+            Em 12x s/ juros de{" "}
+            {(product.price / 12).toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "brl",
+            })}
+          </span>
         </div>
+
         <div className="mt-8 space-y-4">
           <span className="block font-semibold">Tamanhos</span>
           <div className="flex gap-2">
