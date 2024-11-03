ad80890eb6018d8bc6f9c7726440013416da0c03
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-11-03 16:38:38 -0300

    add github workflow

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,7 +1,7 @@
 describe("search products", () => {
   it("should be able to search for products", () => {
     cy.visit("/");
-    cy.get("input[name=q]").type("moletom").parent("form").submit();
+    cy.searchByQuery("moletom");
     cy.location("pathname").should("include", "/search");
     cy.location("search").should("include", "q=moletom");
     cy.get('a[href^="/product"]').should("exist");
@@ -11,7 +11,7 @@ describe("search products", () => {
     cy.on("uncaught:exception", () => {
       return false;
     });
-    
+
     cy.visit("/search");
     cy.location("pathname").should("equal", "/");
   });
