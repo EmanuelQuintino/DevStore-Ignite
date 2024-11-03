ee4ef426daff7daf6382061672f70c2e882edf11
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-11-03 16:54:37 -0300

    add github workflow

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 import { Product } from "@/data/types/product";
 import { api } from "@/data/api";
+import { Suspense } from "react";
+import SearchLoading from "./loading";
 
 interface SearchProps {
   searchParams: {
@@ -31,43 +33,45 @@ export default async function Search({ searchParams }: SearchProps) {
   const products = await searchProducts(query);
 
   return (
-    <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query}</span>
-      </p>
+    <Suspense fallback={<SearchLoading />}>
+      <div className="flex flex-col gap-4">
+        <p className="text-sm">
+          Resultados para: <span className="font-semibold">{query}</span>
+        </p>
 
-      <div className="grid grid-cols-3 gap-6">
-        {products.map((product) => {
-          return (
-            <Link
-              key={product.id}
-              href={`/product/${product.slug}`}
-              className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-            >
-              <Image
-                src={product.image}
-                className="group-hover:scale-105 transition-transform duration-500"
-                width={480}
-                height={480}
-                quality={100}
-                alt=""
-              />
+        <div className="grid grid-cols-3 gap-6">
+          {products.map((product) => {
+            return (
+              <Link
+                key={product.id}
+                href={`/product/${product.slug}`}
+                className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+              >
+                <Image
+                  src={product.image}
+                  className="group-hover:scale-105 transition-transform duration-500"
+                  width={480}
+                  height={480}
+                  quality={100}
+                  alt=""
+                />
 
-              <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-                <span className="text-sm truncate">{product.title}</span>
-                <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-                  {product.price.toLocaleString("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
-                    minimumFractionDigits: 0,
-                    maximumFractionDigits: 0,
-                  })}
-                </span>
-              </div>
-            </Link>
-          );
-        })}
+                <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                  <span className="text-sm truncate">{product.title}</span>
+                  <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                    {product.price.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                      minimumFractionDigits: 0,
+                      maximumFractionDigits: 0,
+                    })}
+                  </span>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
